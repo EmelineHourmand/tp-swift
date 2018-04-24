7127b991ac010c9e7fcd667b20cd1035cb63ad93
user2 <user2@Mac-mini-de-mac.local>
2018-04-24 17:08:38 +0200

    init project

---
+++ TableViewAppTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TableViewAppTest
+//
+//  Created by user2 on 24/04/2018.
+//  Copyright © 2018 user2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var tableView: UITableViewCell!
+}
+
